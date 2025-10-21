23aff608ca0203fa40b012caadf6f35241e24a5c
Azeem Rajput <muhammadaxeem24@gmail.com>
2025-10-22 02:03:15 +0500

    added initial code

---
+++ javascript/conversion.js
@@ -0,0 +1,28 @@
+let score="33"
+//console.log(typeof score)
+
+let valueInNumber=Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+//"33" => 33
+//"abc" => NaN (stand for Not an Number)
+//undefined => NaN
+//null => 0
+//true => 1
+//false => 0
+
+let isLogged=""
+let stringBolean=Boolean(isLogged)
+console.log(stringBolean)
+console.log(typeof stringBolean)
+
+//1 => true
+//0 => false
+//"" => false
+//"anything inthe comas" => true
+
+let isNumber=33
+let intoString=String(isNumber)
+console.log(intoString)
+
