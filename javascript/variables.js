8a7b3d4724029e25bb34208e95f221cc40cce0c3
Azeem Rajput <muhammadaxeem24@gmail.com>
2025-10-22 00:00:29 +0500

    added initial code

--- javascript/variables.js
+++ javascript/variables.js
@@ -1,1 +1,14 @@
 const accountId=144553
+let accountEmail="azeem@google.com"
+var accountPassward="10oej"
+
+// accountId=5674//not allowed
+
+accountEmail="rajput@google,com"
+accountPassward="111001"
+
+//  console.log (accountId)
+//  console.log (accountEmail)
+// console.log (accountPassward)
+
+console.table([accountId,accountEmail,accountPassward])
