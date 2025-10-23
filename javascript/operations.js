c0a88274e9aa449094012ef07a2b4c835ec19838
Azeem Rajput <muhammadaxeem24@gmail.com>
2025-10-24 00:03:10 +0500

    added initial code

---
+++ javascript/operations.js
@@ -0,0 +1,17 @@
+let num=3
+let negValue=-num
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(3%2)
+// console.log(2**3)
+
+let nOne="Azeem"
+let nTwo=" Rajput"
+let fullName=nOne+nTwo
+// console.log(fullName)
+
+console.log(3+4*5%3)
