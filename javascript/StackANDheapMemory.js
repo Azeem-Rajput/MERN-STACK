baaa7d2abfe3c0131de9f96f55f425b4f97fb738
Azeem Rajput <muhammadaxeem24@gmail.com>
2025-10-24 02:59:38 +0500

    added initial code

---
+++ javascript/StackANDheapMemory.js
@@ -0,0 +1,17 @@
+// stack(In primitive data types)  
+let n1=123
+let n2=n1
+n2=456
+// console.log(n1)
+// console.log(n2)
+
+//& heap(In non-primitive)
+
+let m1={
+    email:"azeem@google",
+    passward:123
+}
+let m2=m1   
+m2.email="rajput@google"
+console.log(m1)
+console.log(m2)
---
+++ javascript/string.js
@@ -0,0 +1,45 @@
+let name="ares "
+let rollNo=23
+// console.log(name+rollNo)
+
+console.log(`hello my name is ${name}and my roll number is ${rollNo}`)
+
+const game=new String('Azeem')
+
+console.log(game[3])
+console.log(game.__proto__)  // looks empty {}
+// console.dir(game.__proto__);  // shows full prototype object
+//                or
+// console.log(Object.getOwnPropertyNames(game.__proto__));// shows full prototype object
+
+
+
+
+// __proto__ i the all prototypes of object (like the string which we use its holds diff. prototypes like uppercase letter, lenght that we call below....)
+// console.log(game.length)
+console.log(game.toUpperCase())
+console.log(game.charAt(2))
+console.log(game.indexOf('e'))
+
+const newString=game.substring(0,3)
+console.log(newString)
+
+const anotherString=game.slice(1,-2)
+console.log(anotherString);
+
+const lushString="   azeem    "
+console.log(lushString.trimEnd())
+
+const url="http://azeem.com/azeem%20ans100"
+console.log(url.replace('100','---'))
+
+console.log(url.includes('rajput'))
+console.log(url.includes('azeem'))
+
+console.log(game.split('-'))
+
+
+
+
+
+
