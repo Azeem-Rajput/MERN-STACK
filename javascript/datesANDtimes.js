ea072465b95b5ef5c1fff027c8f70f04f3c3ee45
Azeem Rajput <muhammadaxeem24@gmail.com>
2025-10-31 03:03:20 +0500

    added initial code

---
+++ javascript/datesANDtimes.js
@@ -0,0 +1,17 @@
+// dates
+let theDates=new Date()
+
+console.log(typeof theDates);
+
+// console.log(theDates.toString())
+// console.log(theDates.toISOString())
+// console.log(theDates.toJSON())
+// console.log(theDates.toLocaleDateString())
+// console.log(theDates.toLocaleString())
+// console.log(theDates.toLocaleTimeString())
+
+let fixDate=new Date(2025,2,23)
+// console.log(fixDate.toLocaleDateString())
+
+let otherDate=new Date("2025-01-22")
+console.log(otherDate.toLocaleDateString())
