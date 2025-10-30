b3ddc43bf00c162728692f44a8bf0a7a428e4a70
Azeem Rajput <muhammadaxeem24@gmail.com>
2025-10-30 23:26:44 +0500

    added initial code

--- javascript/numsANDmaths.js
+++ javascript/numsANDmaths.js
@@ -3,3 +3,12 @@ console.log(score)
 
 const oneMore=new Number(500)
 console.log(oneMore)
+
+console.log(oneMore.toString().at(0))
+console.log(oneMore.toFixed(5))
+
+const no=132.68
+console.log(no.toPrecision(9))
+
+const value=200000
+console.log(value.toLocaleString('en-IN'))
