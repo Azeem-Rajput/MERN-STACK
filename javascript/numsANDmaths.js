0b367c35de6ee25b32d13eef640e75a1ec0e5a4e
Azeem Rajput <muhammadaxeem24@gmail.com>
2025-10-31 00:12:50 +0500

    added initial code

--- javascript/numsANDmaths.js
+++ javascript/numsANDmaths.js
@@ -1,14 +1,37 @@
+//++++++++++++++++Numbers+++++++++++++++++++++
 const score=400
-console.log(score)
+// console.log(score)
 
 const oneMore=new Number(500)
-console.log(oneMore)
+// console.log(oneMore)
 
-console.log(oneMore.toString().at(0))
-console.log(oneMore.toFixed(5))
+// console.log(oneMore.toString().at(0))
+// console.log(oneMore.toFixed(5))
 
 const no=132.68
-console.log(no.toPrecision(9))
+// console.log(no.toPrecision(9))
 
 const value=200000
-console.log(value.toLocaleString('en-IN'))
+// console.log(value.toLocaleString('en-IN'))
+Number
+// console.log(Number.EPSILON)
+
+
+// ++++++++++++++++++Maths+++++++++++++++++
+// console.log(Math)
+// console.log(Math.abs(-19))
+// console.log(Math.cbrt(64))
+// console.log(Math.round(4.5))
+// console.log(Math.ceil(4.1))
+// console.log(Math.floor(4.9))
+// console.log(Math.min(200,900,100000,200000))
+// console.log(Math.max(200,900,100000,200000))
+
+console.log(Math.random())
+console.log(Math.random()*10+1)
+console.log((Math.random()*10)+1)
+console.log(Math.floor((Math.random()*10)+1))
+
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1))+min)
