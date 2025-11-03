3a5b4cc11d47fbaa32ce1245ad83eb58ec6ddecc
Azeem Rajput <muhammadaxeem24@gmail.com>
2025-11-03 23:08:10 +0500

    added initial code

---
+++ js/array.js
@@ -0,0 +1,23 @@
+//array
+const myArray=[3,4,7,2,5]
+// console.log(myArray[2])
+
+const array2=new Array(1,5,2,8,3)
+// console.log(array2[3])
+
+// myArray.push(8)
+// myArray.push(10)
+// myArray.pop()
+// myArray.unshift(11)
+// myArray.unshift(1)
+// myArray.shift()
+// myArray.shift()
+// console.log(myArray.indexOf(10))
+// console.log(myArray.includes(10))
+
+const anotherArray=myArray.join()
+console.log(myArray)
+console.log(anotherArray)
+console.log(typeof anotherArray)
+console.log(typeof myArray)
+``
