510803c52ca0efa3df4c91be100a9778f2c13f67
Azeem Rajput <muhammadaxeem24@gmail.com>
2025-11-04 02:10:33 +0500

    added initial code

--- js/object.js
+++ js/object.js
@@ -2,3 +2,12 @@
 // object.create ( also known as "through constructor")
 
 // object literals
+const JsUser={
+    name:"Muhammad Azeem",
+    age:18,
+    location:"kdj",
+    logIn:"azeem@software.com",
+    logInDay:["monday","thursday"]
+}
+console.log(JsUser.name)  // its not valid way
+console.log(JsUser["age"]) // another way    /* In behind name,age.location,etc all objects treat as string */
