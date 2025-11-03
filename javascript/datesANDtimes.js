2ace997369a40159fb6f870757cdc13e3ee49523
Azeem Rajput <muhammadaxeem24@gmail.com>
2025-11-03 21:52:01 +0500

    added initial code

--- javascript/datesANDtimes.js
+++ javascript/datesANDtimes.js
@@ -1,7 +1,7 @@
 // dates
 let theDates=new Date()
 
-console.log(typeof theDates);
+// console.log(typeof theDates);
 
 // console.log(theDates.toString())
 // console.log(theDates.toISOString())
@@ -14,4 +14,19 @@ let fixDate=new Date(2025,2,23)
 // console.log(fixDate.toLocaleDateString())
 
 let otherDate=new Date("2025-01-22")
-console.log(otherDate.toLocaleDateString())
+// console.log(otherDate.toLocaleDateString())
+
+let timeStamp=Date.now()
+// console.log(timeStamp)
+// console.log(otherDate.getTime())
+
+// console.log(Date.now())  //to convert it in to secands so divided by 1000
+// console.log(Math.floor(Date.now()/1000))
+
+let date=new Date()
+console.log(date.getMonth()+1)
+console.log(date.getFullYear())
+
+date.toDateString('dafault',{
+  fixDate: "long"
+})
