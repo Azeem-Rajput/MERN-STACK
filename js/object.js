41071a7cdfe6741237846387d5f110b05350711d
Azeem Rajput <muhammadaxeem24@gmail.com>
2025-11-04 02:25:10 +0500

    added initial code

--- js/object.js
+++ js/object.js
@@ -2,8 +2,12 @@
 // object.create ( also known as "through constructor")
 
 // object literals
+
+const mySymb=Symbol("cluster")
 const JsUser={
     name:"Muhammad Azeem",
+    "full name":"M-A-R",
+    [mySymb]:"key1",   // syntex of using symbol in object
     age:18,
     location:"kdj",
     logIn:"azeem@software.com",
@@ -11,3 +15,5 @@ const JsUser={
 }
 console.log(JsUser.name)  // its not valid way
 console.log(JsUser["age"]) // another way    /* In behind name,age.location,etc all objects treat as string */
+console.log(JsUser["full name"])
+console.log(typeof JsUser[mySymb])
