2fc62b8ae7a1156f136d229520f616f7b9508b89
Azeem Rajput <muhammadaxeem24@gmail.com>
2025-11-07 01:23:57 +0500

    added initial code

--- js/object.js
+++ js/object.js
@@ -7,15 +7,31 @@ const mySymb=Symbol("cluster")
 const JsUser={
     name:"Muhammad Azeem",
     "full name":"M-A-R",
-    [mySymb]:"key1",   // syntex of using symbol in object
+    [mySymb]:"key1",   // syntex of using symbol in object is[]
     age:18,
     location:"kdj",
     logIn:"azeem@software.com",
     logInDay:["monday","thursday"]
 }
 
-console.log(JsUser.name)  // its not valid way
-console.log(JsUser["age"]) // another way    /* In behind name,age.location,etc all objects treat as string */
+// console.log(JsUser.name)  // its not valid way
+// console.log(JsUser["age"]) // another way    /* In behind name,age.location,etc all objects treat as string */
 
-console.log(JsUser["full name"])
-console.log(typeof JsUser[mySymb])
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySymb])
+// console.log(typeof mySymb)
+
+// JsUser.logIn="azeem@gpt.com"
+// console.log(JsUser)
+// Object.freeze()
+// JsUser.logIn="azeem@softs.com"
+
+// defining function
+JsUser.info=function(){
+    console.log("Arm wrestler")
+}
+JsUser.infotwo=function(){
+    console.log(`Arm wrestler,${this.name}`)
+}
+JsUser.info() 
+JsUser.infotwo() 
