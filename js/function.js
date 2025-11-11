b48a908b3a57f567e2fa24f6ae997f3a23c11ef1
Azeem Rajput <muhammadaxeem24@gmail.com>
2025-11-12 03:35:59 +0500

    added initial code

--- js/function.js
+++ js/function.js
@@ -24,13 +24,59 @@ const result=addNumbers(2,3)    // .................when value are passing then 
 function weLogin(username)  // we use as a default like (username="kka")
 {
     if(!username)  // ---------if(username==undefined)
-    {
-        console.log("Enter your username")
+    { 
+        // console.log("Enter your username")
         return
     }
     return `${username} is logged in`
 }
-console.log (weLogin())   //  weLogin()
+// console.log (weLogin())   //  weLogin()
 
+
+
+
+
+function countingItemOfstore(...num)  // Rest operator(...) mens how multiple values passes through function
+{
+    return num
+}
+// console.log(countingItemOfstore(1000,2000,3000))
+
+
+
+
+
+
+// ----------------------Access object in function-------------------------
+const obj1={
+    realName:"Azeem",
+    id:937
+}
+
+function obf(object)  // we pass value is must me object data type e.g(object)
+{
+    // console.log(`the real name is=${object.realName} and the unique id is=${object.id}`)
+}
+// obf(obj1)
+obf({
+    realName:"Rajput",
+    id:22000
+})
+
+
+
+
+
+
+
+//------------------------Access array in function----------------------
+const array=[29,900,11]
+
+function withArray(otherArray)
+{
+    return `The secand value of aaray is ${otherArray[1]}`
+}
+// console.log(withArray(array))
+console.log(withArray([100,300,7100]))
 
 
