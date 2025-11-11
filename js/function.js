d194af07cd8825e44ded8c570cd8a6fc78bb65c1
Azeem Rajput <muhammadaxeem24@gmail.com>
2025-11-12 02:42:16 +0500

    added initial code

--- js/function.js
+++ js/function.js
@@ -4,7 +4,33 @@ function sayMyname(){
 }
 // sayMyname()
 
-function addNumbers(num1,num2){
-    console.log(num1+num2)
+// function addNumbers(num1,num2)  //.............. { (num1 & num2) are parametrs(when num1 and num2 are form ) } 
+// {
+//     console.log(num1+num2)
+// }
+
+
+function addNumbers(num1,num2)
+{
+    // console.log("kjdsuh")
+    return num1+num2
+    console.log("wnj")
 }
-addNumbers(2,undefined)
+const result=addNumbers(2,3)    // .................when value are passing then is known as arguments ( when we call function )
+// console.log(result)
+
+
+
+function weLogin(username)  // we use as a default like (username="kka")
+{
+    if(!username)  // ---------if(username==undefined)
+    {
+        console.log("Enter your username")
+        return
+    }
+    return `${username} is logged in`
+}
+console.log (weLogin())   //  weLogin()
+
+
+
