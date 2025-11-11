ecd54cc4ed225c8f44bc3d233bf3023e53c51fba
Azeem Rajput <muhammadaxeem24@gmail.com>
2025-11-12 04:19:15 +0500

    added initial code

---
+++ js/scope.js
@@ -0,0 +1,44 @@
+var c=100       //   ( Global Scope)
+let a=200
+
+if (true)       //  (
+{
+    let a=2     //       Block Scope
+    const c=99  //  )
+}
+// console.log(a)
+//      console.log(a)
+// console.log(c)
+
+
+
+function onee()
+{
+    namea="AZeem"
+    function twoo()
+    {
+        nameb="RAjput"
+        // console.log(namea)
+    }
+    // console.log(nameb)
+
+    twoo()
+}
+onee()
+
+
+
+
+// --------------------Intresting--------------------------
+addingOne(9)
+function addingOne(num)
+{
+    return num+1
+}
+
+two(11)
+const two=function(num)
+{
+    return num+2
+}
+
