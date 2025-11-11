55592e71db9db87917554f923cf1fcb8f383833c
Azeem Rajput <muhammadaxeem24@gmail.com>
2025-11-12 02:11:01 +0500

    added initial code

--- js/function.js
+++ js/function.js
@@ -2,3 +2,9 @@ function sayMyname(){
     console.log("Azeem")
     console.log("Rajput")
 }
+// sayMyname()
+
+function addNumbers(num1,num2){
+    console.log(num1+num2)
+}
+addNumbers(2,undefined)
