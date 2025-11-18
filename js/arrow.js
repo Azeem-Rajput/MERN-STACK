2aaed4c4ee9393d8ddb58073ac43a5e20ddc2537
Azeem Rajput <muhammadaxeem24@gmail.com>
2025-11-19 02:09:20 +0500

    added initial code

--- js/arrow.js
+++ js/arrow.js
@@ -30,3 +30,29 @@ const customer={
 //     console.log(this.username)   // Wed see that "this" is only works in object
 // }
 // chaye() 
+
+
+const chaye= ()=>
+{
+    let username="aazeem"
+    // console.log(this.username)   // Wed see that "this" is only works in object
+}
+chaye() 
+
+//----------------------Explisit return(when curly braces use)-------------------
+
+// const adding=(num1,num2)=>{
+//     return num1+num2;
+// }
+// console.log(adding(2,2))
+
+// ++++++++++++++++++++Implisit return(when no any curly braces use)
+// const adding=(num1,num2) =>  (num1+num2)  // with no curly braces
+// console.log(5+5)
+
+
+// ...........for object.............
+
+const add=(num1 , num2) => ({nameo:"Azeem Rajput"})
+
+console.log(add(2 , 7))
