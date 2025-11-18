8eceb7df5d793bcba60e2721d00a3b795b12cd23
Azeem Rajput <muhammadaxeem24@gmail.com>
2025-11-19 01:36:30 +0500

    added initial code

--- js/arrow.js
+++ js/arrow.js
@@ -3,9 +3,30 @@ const customer={
     pass:120,
     print:function()
     {
-        console.log(`${this.name}--welcome to account`)
+        // console.log(`${this.name}--welcome to account`)
+        // console.log(this)  // "this" --> talk about current values
     }
 }
-customer.print()
-customer.name="Rajput"
-customer.print()
+// customer.print()
+// customer.name="Rajput"
+// customer.print()
+
+// console.log(this)  // if it check in browser then it is "Window" object
+
+
+
+// function chaye()
+// {
+//     let username="aazeem"
+//     console.log(this.username)   // Wed see that "this" is only works in object
+// }
+// chaye() 
+
+
+
+// const chaye=function ()
+// {
+//     let username="aazeem"
+//     console.log(this.username)   // Wed see that "this" is only works in object
+// }
+// chaye() 
