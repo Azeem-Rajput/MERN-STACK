043f9e0857857edeb5c3639447255f5d4413aeae
Azeem Rajput <muhammadaxeem24@gmail.com>
2026-01-07 02:25:47 +0500

    added initial code

---
+++ js/IIFE.js
@@ -0,0 +1,4 @@
+//   Immediately Invoked Function Expression (IIFE)
+    // " IIFE is used for avoidens of global varibles which are located in global scope in some functions which are used"  
+ // "used for avoid pollution of global scope"
+ 
