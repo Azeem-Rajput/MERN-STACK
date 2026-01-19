f4de6fe8eb6a3da8c4f1a442b6d9308ec91629ad
Azeem Rajput <muhammadaxeem24@gmail.com>
2026-01-20 02:59:01 +0500

    added code

--- Control_flow-js/one.js
+++ Control_flow-js/one.js
@@ -14,17 +14,55 @@ if (isLoggedIn)
 
 // < , > , <= , >= , != ,==(only check values) ,===(also check data types), !== 
 
-if(2=="2")
+// if(2=="2")
+// {
+//     console.log("executed")
+// }
+
+// if(2==="2")
+// {
+//     console.log("executed")
+// }
+
+// if(2!=="2")
+// {
+//     console.log("executed")
+//  }
+
+// else
+// {
+//     console.log("third statement not executed")
+// }
+
+
+// const score=20
+// if(score<40)
+// {
+//     const code="o2n"
+//     console.log(`the hidden code is=${code}`)
+// }
+// console.log(`the hidden code is=${code}`)
+
+
+
+const amount=200
+// if(amount>100) console.log("yes"),console.log("yesss")      immature style
+
+
+
+
+const creditCard=true
+const debitCard=true
+const loggedInfromgoogle=false
+const loggedInfromemail=true
+
+if(creditCard && debitCard)
 {
-    console.log("executed")
+    console.log("you are eligible")
+}
+if(loggedInfromemail || loggedInfromgoogle)
+{
+    console.log("valid")
 }
 
-if(2==="2")
-{
-    console.log("executed")
-}
-
-if(2!=="2")
-{
-    console.log("executed")
-}
+// ++++++++++ conditional randering +++++++++++
