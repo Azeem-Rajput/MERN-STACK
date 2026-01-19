b0492c5406d628a61fd5da75c7080b2d36b8fc9e
Azeem Rajput <muhammadaxeem24@gmail.com>
2026-01-20 02:30:36 +0500

    added initial code

---
+++ Control_flow-js/one.js
@@ -0,0 +1,30 @@
+// ------------------ if ------------------ 
+
+// if(condition)
+// {
+//     // scope
+// }
+
+const isLoggedIn=true
+
+if (isLoggedIn)
+{
+    // if condition is true than scope works 
+}
+
+// < , > , <= , >= , != ,==(only check values) ,===(also check data types), !== 
+
+if(2=="2")
+{
+    console.log("executed")
+}
+
+if(2==="2")
+{
+    console.log("executed")
+}
+
+if(2!=="2")
+{
+    console.log("executed")
+}
