05ddf5b3ea5aa359ebfa56059966fcf9047482ad
Azeem Rajput <muhammadaxeem24@gmail.com>
2026-02-01 23:48:15 +0500

    added initial code

--- 04_Iterations/one.js
+++ 04_Iterations/one.js
@@ -37,7 +37,8 @@ for (let index = 0; index < theArray.length; index++) {
 
 
 // _________________________________break & continue_________________________________
-for (let index = 0; index <=20 ; index++) {
+for (let index = 0; index <=20 ; index++)
+     {
     
     if(index==5)
     {
