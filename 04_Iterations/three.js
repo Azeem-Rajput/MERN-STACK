ad1a94f6029b6d5e42bf0209e8808b871269f5c6
Azeem Rajput <muhammadaxeem24@gmail.com>
2026-03-17 03:12:04 +0500

    added initial code

---
+++ 04_Iterations/three.js
@@ -0,0 +1,19 @@
+const av=[1,2,3,4,5]
+for (const i of av) {
+//    console.log(i)  
+}
+
+
+
+const va=['hello world']
+for (const i of va) {
+   // console.log(i)
+}
+
+
+
+
+const greeting='hello guys'
+for (const i of greeting) {
+    console.log(i)
+}
