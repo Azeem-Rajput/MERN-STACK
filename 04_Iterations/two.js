9a2f932334c368d055e0ef4ae00984dafc701df0
Azeem Rajput <muhammadaxeem24@gmail.com>
2026-03-17 01:54:54 +0500

    added initial code

---
+++ 04_Iterations/two.js
@@ -0,0 +1,32 @@
+//---------------- while loop ---------------
+// let num=1
+// while(num<=10)
+// {
+//     // console.log(`the values are ${num}`)
+//     num=num+1
+// }
+
+
+const arr=["azeem","rajput"]
+let num=0
+while(num<arr.length)
+{
+ //   console.log(arr[num])
+    num=num+1
+}
+
+// ++++++++++++++++++ do while ++++++++++++++
+
+
+let n=0
+do{
+   // console.log(n)
+    n=n+1
+}while(n<=10)
+
+
+let m=2
+do{
+    console.log(m)
+    m=m+1
+}while(m<=1)    
