9d08ac9a056521d1830383e00e8e3c6acb4abd4f
Azeem Rajput <muhammadaxeem24@gmail.com>
2026-03-18 23:59:23 +0500

    added js code

---
+++ js/04_Iterations/five.js
@@ -0,0 +1,45 @@
+// now we dicuss FOR EACH loop
+
+const array=['arm','wresling']
+// array.forEach( function (val) {
+//     console.log(val)
+// } )
+
+                     // for arrow function
+array.forEach( (val)=>{
+    //console.log(val)
+} )
+
+
+// function some(item)
+// {
+//    // console.log(item)
+// }
+// array.forEach( some )
+
+
+// array.forEach( (item,index,arr)=>{
+//     console.log(item,index,arr)
+// } )
+
+
+
+
+
+
+
+const array2=[
+    {
+        js:'java',
+        file:'js'
+    },
+    {
+        js:'python',
+        file:'py'
+    },
+    {
+        js:'css',
+        file:'cpp'
+    }
+]
+
--- js/04_Iterations/four.js
+++ js/04_Iterations/four.js
@@ -15,7 +15,16 @@ for (const key in myobj) {
 // so we check array
 const array=['azeem','rajput']
 for (const key in array) {
-    console.log(key)
-    console.log(array[key])
+//    console.log(key)
+  //  console.log(array[key])
 }
 
+const map=new Map()
+map.set('UK',"United kingdom")
+map.set('USA',"United State Of America")
+map.set('KSA',"Kingdom Of Saudia Arabia")
+map.set('UK',"United kingdom")
+
+for (const key in map) {
+    console.log(map)                  // its not giving anything because map is not iteratable
+}
