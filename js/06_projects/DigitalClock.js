472a4b9628848b93952c4a5a6a0120e1cd5f81dc
Azeem Rajput <muhammadaxeem24@gmail.com>
2026-04-17 18:09:23 +0500

    added js code

--- js/06_projects/DigitalClock.js
+++ js/06_projects/DigitalClock.js
@@ -1,5 +1,5 @@
 const clock=document.getElementById('clock')
-setInterval(function(){
+setInterval(function() {
    let date=new Date()
    clock.innerHTML=date.toLocaleTimeString()         
  } , 1000)
