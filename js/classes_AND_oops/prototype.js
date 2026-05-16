058b0320483df6996a975870c1d45877e8d0f85a
Azeem Rajput <muhammadaxeem24@gmail.com>
2026-05-16 05:10:49 +0500

    added js prototypes

---
+++ js/classes_AND_oops/prototype.js
@@ -0,0 +1,65 @@
+// let myName="azeem"
+// let mynameTwo="rajput"
+
+// console.log(myName.truename)
+
+
+let myHeros=['thor','spiderman']
+let heroPowers={
+    thor:"hammer",
+    spiderman:"sling",
+
+    getSpiderPower:function()
+    {
+        console.log(`spider power is ${this.spiderman}`)
+    }
+
+}
+
+Object.prototype.hitesh=function(){
+    console.log(`hitesh teaching js on his channel`)
+}
+Array.prototype.hitesho=function(){
+    console.log(`hitesh is chaudhary`)
+}
+
+myHeros.hitesh()
+heroPowers.hitesh()
+myHeros.hitesho()
+// heroPowers.hitesho()
+
+
+const User = {
+    name: "chai",
+    email: "chai@google.com"
+}
+
+const Teacher = {
+    makeVideo: true
+}
+
+const TeachingSupport = {
+    isAvailable: false
+}
+
+const TASupport = {
+    makeAssignment: 'JS assignment',
+    fullTime: true,
+    __proto__: TeachingSupport
+}
+
+Teacher.__proto__ = User
+
+// modern syntax
+Object.setPrototypeOf(TeachingSupport, Teacher)
+
+let anotherUsername = "ChaiAurCode     "
+
+String.prototype.trueLength = function(){
+    console.log(`${this}`);
+    console.log(`True length is: ${this.trim().length}`);
+}
+
+anotherUsername.trueLength()
+"hitesh".trueLength()
+"iceTea".trueLength()
