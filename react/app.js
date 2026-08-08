8778c7715da8f79453f703e9dd8156b47f42e209
Azeem Rajput <muhammadaxeem24@gmail.com>
2026-08-08 10:04:55 +0500

    added react code

--- react/app.js
+++ react/app.js
@@ -6,6 +6,8 @@
 //            }
 // }   
 
+const { createElement } = require("react")
+
 // element={
 //     tag:h1,
 //     textContent:
@@ -61,7 +63,69 @@
 // }
 
 
-const element1=React.createElement("h1",{className:"element",id:"first",style:{fontSize:"20px",backgroundColor:"green",color:"white"}},"hello coder army (azeem rajput)")
 
-const root=document.getElementById('root')
-ReactDOM.render(element1,root)
+// original react
+const React={    
+    createElement: function(type,props,children)
+    {
+        return{
+            type:type,
+            props:{
+                ...props,
+                children:children
+            }
+        }
+    }
+}
+
+// const reactElement={
+//     type:'h1',
+//     props:{
+//         className:"element",
+//         id:"first",
+//         style:{fontSize:"20px",backgroundColor:"green",color:"white"},
+//         children:"hello coder army (azeem rajput)"
+//     }
+// }
+
+const ReactDOM={
+    render:function(reactElement,root)
+    {
+        const element=document.createElement(reactElement.type);
+        
+        const {props}=reactElement;
+
+        for(const key in props)
+        {
+            if(key==style)
+            {
+            Object.assign(element.style,props.style)
+            }
+            else if(key ==children)
+            {
+                element.textContent=props[key]
+            }
+            else
+            {
+                element[key]=props[key]
+            }
+        }
+        root.append(element); 
+
+
+    }
+}
+
+const element1=React.createElement("h1",{className:"element",id:"first",style:{fontSize:"20px",backgroundColor:"green",color:"white"}},"hello coder army (azeem rajput)")
+const element2=React.createElement("h2",{className:"element",id:"secand",style:{fontSize:"20px",backgroundColor:"orange",color:"white"}},"start react (azeem rajput)")
+
+console.log(element1)
+// ReactDOM.render(element1,document.getElementById('root'));
+
+const root=ReactDOM.createRoot(document.getElementById('root'));
+root.render(div);
+
+
+// const root=document.getElementById('root')
+// // ReactDOM.render(element1,root)
+// ReactDOM.render(element2,root)
