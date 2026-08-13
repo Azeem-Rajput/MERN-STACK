494452ed55ae21eea029d89d4765e83b0f450e2d
Azeem Rajput <muhammadaxeem24@gmail.com>
2026-08-13 07:31:40 +0500

    added react code

---
+++ react/three/app.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+
+
+const element=<h1>helloo g coder</h1>
+ReactDOM.createRoot(document.getElementById('root')).render(element)           // vite have its ES-Build which in place of bable
