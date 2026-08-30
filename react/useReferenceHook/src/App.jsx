b850efc1722b72eddb3d38fd15843ddeb66659f0
Azeem Rajput <muhammadaxeem24@gmail.com>
2026-08-30 07:10:15 +0500

    added useRef hook

--- react/useReferenceHook/src/App.jsx
+++ react/useReferenceHook/src/App.jsx
@@ -1,19 +1,33 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 
 function App() {
   const [time, setTime] = useState(0)
 
+  const intervalIdRef=useRef(null);
+
   function handleStart(){
-    setInterval(()=>{
-      setTime(time+1)
+    if(intervalIdRef.current!=null)
+    {
+      return;
+    }
+    intervalIdRef.current=setInterval(()=>{
+      setTime(time=>time+1)
     },1000)
+  }
+  function handleStop(){
+    clearInterval(intervalIdRef.current);
+    intervalIdRef.current=null;
+  }
+  function handleReset(){
+    clearInterval(intervalIdRef.current);
+    setTime(0);
   }
   return (
     <>
      <h1>Stopwatch: {time}</h1>
      <button onClick={handleStart}>start</button>
-     <button>stop</button>
-     <button>reset</button>
+     <button onClick={handleStop}>stop</button>
+     <button onClick={handleReset}>reset</button>
     </>
   )
 }
