9c8b1f5a32e1b344f8133667a192bad63cbd2fdf
Azeem Rajput <muhammadaxeem24@gmail.com>
2026-09-11 08:11:40 +0500

    added navbar

--- frontendProjects/src/App.jsx
+++ frontendProjects/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
-
+import Navbar from './components/Navbar'
 function App() {
-  const [count, setCount] = useState(0)
+  const [theme, setTheme] = useState('light')
 
   return (
     <>
-      
+      <div className='dark:bg-black relative'>
+        <Navbar theme={theme} setTheme={setTheme}></Navbar>
+      </div>
     </>
   )
 }
