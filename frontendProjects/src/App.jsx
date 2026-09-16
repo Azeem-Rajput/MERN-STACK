04daab1a0e6c91c889c4ff1e5b79384c7352418b
Azeem Rajput <muhammadaxeem24@gmail.com>
2026-09-16 22:55:23 +0500

    added hero section

--- frontendProjects/src/App.jsx
+++ frontendProjects/src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar'
+import Hero    from './components/Hero'
+import TrustedBy from './components/TrustedBy'
 function App() {
   const [theme, setTheme] = useState(localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light')
 
@@ -7,6 +9,8 @@ function App() {
     <>
       <div className='dark:bg-black relative'>
         <Navbar theme={theme} setTheme={setTheme}></Navbar>
+        <Hero />
+        <TrustedBy />
       </div>
     </>
   )
